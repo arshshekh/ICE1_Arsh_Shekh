e71f83520dd2804af279dab24f5da48f754b63c8
shekhars <shekhars@sheridancollege.ca>
2023-05-25 18:44:33 -0400

    Arsh Branch

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,11 +4,15 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * @author arshshekh  StudentId: 991676386
+ * 
  */
 public class CardTrick {
     
@@ -19,13 +23,24 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int)(1+Math.random()*13));
+            c.setSuit(Card.SUITS[(int)(0+Math.random()*4)]);
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            magicHand [i] = c;
+            
         }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please pick any card:");
+        int CardValue = input.nextInt();
         
+        System.out.println("Please pick the suit:");
+        String CardSuit = input.next();
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        System.out.println("You picked "+CardValue+ "and suit" + CardSuit );
     }
     
 }
